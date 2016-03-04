3adceca3c7c56ace0e1a46693984cc3317e4d28a
Dimitar Uzunov <dimitar.uzunov.dev@gmail.com>
2016-03-04 19:07:16 +0200

    Add solution to second part of first problem. 2D point structure with methods

---
+++ week2/problem1/point_2d_v2.cpp
@@ -0,0 +1,51 @@
+#include <iostream>
+#include <cmath>
+
+using namespace std;
+
+struct Point2D {
+  double x;
+  double y;
+
+  void translate(double dx, double dy) {
+    x += dx;
+    y += dy;
+  }
+
+  double distanceTo(Point2D other) {
+    return sqrt(pow(x - other.x, 2) + pow(y - other.y, 2));
+  }
+
+  void print() {
+    cout << '(' << x << ", " << y << ')' << '\n';
+  }
+};
+
+
+void testTranslate() {
+  Point2D point;
+  point.x = 3;
+  point.y = 5;
+
+  point.translate(2, 3);
+  point.print();
+}
+
+void testDistanceBetween() {
+  Point2D firstPoint;
+  firstPoint.x = 3;
+  firstPoint.y = 5;
+
+  Point2D secondPoint;
+  secondPoint.x = 4;
+  secondPoint.y = 1;
+
+  cout << firstPoint.distanceTo(secondPoint) << '\n';
+}
+
+int main() {
+  testTranslate();
+  testDistanceBetween();
+
+  return 0;
+}
