feaf1bd2f4db6ede183b16c1bf2493f5022f6cd1
Dimitar Uzunov <dimitar.uzunov.dev@gmail.com>
2016-03-14 15:17:25 +0200

    Add solution to first problem

---
+++ week3/homework/solutions/problem1/client.cpp
@@ -0,0 +1,64 @@
+#include <iostream>
+
+using namespace std;
+
+const int MAX = 100;
+
+struct Person {
+  string firstName;
+  string lastName;
+
+  void input() {
+    cout << "Enter person's first and last names separated by a space: ";
+    cin >> firstName >> lastName;
+  }
+
+  void print() {
+    cout << firstName << ' ' << lastName << '\n';
+  }
+};
+
+struct Client {
+  Person person;
+  double account;
+
+  void input() {
+    person.input();
+
+    cout << "Enter account: ";
+    cin >> account;
+  }
+
+  void print() {
+    person.print();
+    cout << "Account: " << account << '\n';
+  };
+};
+
+void inputClients(int n) {
+  Client clients[MAX];
+
+  for (int i = 0; i < n; ++i) {
+    clients[i].input();
+  }
+}
+
+void printClients(Client clients[], int n) {
+  for (int i = 0; i < n; ++i) {
+    clients[i].print();
+  }
+}
+
+double sumAccounts(Client clients[], int n) {
+  double sum = 0;
+
+  for (int i = 0; i < n; ++i) {
+    sum += clients[i].account;
+  }
+
+  return sum;
+}
+
+int main() {
+  return 0;
+}
